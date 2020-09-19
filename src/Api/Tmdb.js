914c65e1c17bfcb11172291a4e2be958ebbb772b
Hamilton <hamiltonmb.silva@gmail.com>
2020-09-19 11:27:56 -0300

    Criação do projeto Clone da Netflix para estudo

---
+++ src/Api/Tmdb.js
@@ -0,0 +1,13 @@
+const API_KEY = '39d6ba730a535d8ae0dd0a99a2ba1364';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+/*
+-originais da netflix
+-recomendados (trending)
+-em alta (top rated)
+-ação
+-comedia
+-terror
+-romance
+-documentarios
+*/
