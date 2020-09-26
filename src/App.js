0920c956fdef4d57ed870f9a618a3c690ae0f1c8
Hamilton <hamiltonmb.silva@gmail.com>
2020-09-26 13:10:06 -0300

    Criação do componetem FeaturesMovie - filme em destaque, configuração da busca da informação do filme em destaque no Tmdb

--- src/App.js
+++ src/App.js
@@ -2,22 +2,41 @@ import React , { useEffect, useState }from 'react';
 import './App.css';
 import Tmdb from  './Api/Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+
 
 export default () => {
 
+  //criando lista que serão exibidas
   const [movieList, setMovieList] = useState([]);
+
+  //criando os filmes em destaques da tela
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(()=>{
     const loadAll = async () => {
       //Pegando a lista Total
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      //Pegando o filme em destaque
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randowClosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+      let chosen = originals[0].items.results[randowClosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);      
+      
     }
     loadAll();
   }, []);
 
   return(
     <div className="page">
+      
+      {featuredData && 
+        <FeaturedMovie item={featuredData} />
+      }
+
       <section className="lists">
         {movieList.map((item, key)=>(
           <MovieRow  key={key} 
