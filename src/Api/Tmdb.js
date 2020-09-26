a81a54856e0275a1b59f25a7667e962bb9ec11e3
Hamilton <hamiltonmb.silva@gmail.com>
2020-09-26 12:20:27 -0300

    Criação do fundo da pagina, organização da disposição das imagens, colocando efeito ao passar o mouse

--- src/Api/Tmdb.js
+++ src/Api/Tmdb.js
@@ -23,7 +23,7 @@ export default {
         return [
             {
                 slug: 'originals',
-                title: 'Originais da NetFlix',
+                title: 'Originais da Netflix',
                 items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
             },
             {
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React , { useEffect, useState }from 'react';
+import './App.css';
 import Tmdb from  './Api/Tmdb';
+import MovieRow from './components/MovieRow';
 
 export default () => {
 
@@ -11,17 +13,17 @@ export default () => {
       let list = await Tmdb.getHomeList();
       setMovieList(list);
     }
-
     loadAll();
-
   }, []);
+
   return(
     <div className="page">
       <section className="lists">
         {movieList.map((item, key)=>(
-          <div>
-            {item.title}
-          </div> 
+          <MovieRow  key={key} 
+            title={item.title}
+            items={item.items}
+          /> 
         ))}
       </section> 
     </div>
